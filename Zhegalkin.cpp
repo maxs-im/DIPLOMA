3f1957c9d03e05cbfdb76187fac47d2574705d01
Maxim <maxim1998g@gmail.com>
2019-05-14 06:23:21 +0300

    New way for handling errors

---
+++ Zhegalkin.cpp
@@ -0,0 +1,167 @@
+#include "Zhegalkin.h";
+
+template<typename T> using V = std::vector<T>;
+using S = std::string;
+
+Parsing::Error::Error(ErrorsId _id, bool _is_Table, const S& _info = "") 
+	: info(_info), id(_id), is_Table(_is_Table) {};
+void Parsing::Error::set_index(int _index) {
+	index = _index;
+}
+
+S Parsing::Error::id2str(ErrorsId id) {
+	switch (id) {
+	case ErrorsId::IGNORE:
+		return "0 = 0 -> can be ignored";
+	case ErrorsId::INCOMPATIBILITY:
+		return "0 != 1 (incompatibility)";
+	case ErrorsId::GENERAL:
+		return "Incorrect representation";
+	case ErrorsId::NUMBER:
+		return "Incorrect number of variables";
+	case ErrorsId::EMPTY:
+		return "Empty equation";
+	case ErrorsId::DUPLICATE:
+		return "Repeated variables (incompatibility)";
+	case ErrorsId::NONE:
+		return "";
+	default:
+		return "Symbol is not a bit";
+	}
+}
+S Parsing::Error::get_error() const {
+	auto	message = id2str(id),
+			type = S(is_Table ? "Table" : "Full"),
+			prefix = type + " " + std::to_string(index) + ": ";
+	return prefix + message + (info == "" ? "" : " (" + info + ")");
+}
+
+Parsing::Error Parsing::parse_table(const V<S>& tokens) {
+	const auto& table = tokens.back();
+	auto combinations_number = pow(2, tokens.size() - 1);
+	// "x y 11" || "111"
+	if (combinations_number != table.length()) {
+		return Error(Error::ErrorsId::NUMBER, true);
+	}
+
+	S	neg(1, Actions::NEGATIVE),
+		pos(1, Actions::POSITIVE);
+	if (tokens.size() == 1) {
+		// "0"
+		if (tokens[0] == neg)
+			return Error(Error::ErrorsId::IGNORE, true);
+		// "1"
+		else if (tokens[0] == pos)
+			return Error(Error::ErrorsId::INCOMPATIBILITY, true);
+		// "?"
+		else
+			return Error(Error::ErrorsId::GENERAL, true);
+	}
+
+	for (auto chr : table) {
+		// "x y 1?00"
+		if (chr != Actions::NEGATIVE && chr != Actions::POSITIVE) {
+			return Error(Error::ErrorsId::BIT, true, S(1, chr));
+		}
+	}
+
+	std::unordered_set<S> buf_variables(tokens.begin(), tokens.end() - 1);
+	// "x y x 01010101"
+	if (buf_variables.size() != tokens.size() - 1) {
+		return Error(Error::ErrorsId::DUPLICATE, true);
+	}
+
+	// save maybe new variables
+	vocabulary.insert(buf_variables.begin(), buf_variables.end());
+	return Error(Error::ErrorsId::NONE, true);
+}
+
+Parsing::Error Parsing::parse_polynom(const V<S> & tokens) {
+	// "="
+	if (tokens.size() == 1) {
+		return Error(Error::ErrorsId::IGNORE, false);
+	}
+
+	S	add(1, Actions::ADD),
+		eq(1, Actions::EQUAL),
+		neg(1, Actions::NEGATIVE),
+		pos(1, Actions::POSITIVE);
+
+	std::set<S> buf_variables;
+	auto buf_itr(buf_variables.begin());
+
+	// collect variables for current polynom
+	for (const auto& token : tokens) {
+		if (token == add)
+			continue;
+		if (token == eq) {
+			break;
+		}
+		buf_itr = buf_variables.insert(buf_itr, token);
+	}
+
+	// Note: tokens.size > 1
+	auto	prev_t = tokens[tokens.size() - 2],
+		last_t = tokens.back();
+	// "... = ?"
+	if (prev_t == eq) {
+		if (last_t == pos) {
+			// = 1
+			if (buf_variables.size() == 0) {
+				return Error(Error::ErrorsId::INCOMPATIBILITY, false);
+			}
+		}
+		else if (last_t == neg) {
+			// = 0
+			if (buf_variables.size() == 0) {
+				return Error(Error::ErrorsId::IGNORE, false);
+			}
+		}
+		else {
+			// = ???
+			return Error(Error::ErrorsId::BIT, false, last_t);
+		}
+	}
+	else {
+		if (last_t != eq) {
+			return Error(Error::ErrorsId::GENERAL, false);
+		}
+	}
+
+	// save maybe new variables
+	vocabulary.insert(buf_variables.begin(), buf_variables.end());
+	return Error(Error::ErrorsId::NONE, false);
+}
+
+Parsing::Error Parsing::parse_line(const V<S> & line) {
+	if (line.size() == 0) {
+		return Error(Error::ErrorsId::EMPTY, false);
+	}
+
+	S eq(1, Actions::EQUAL);
+	if (std::find(line.begin(), line.end(), eq) != line.end())
+		return parse_polynom(line);
+	else
+		return parse_table(line);
+}
+
+Parsing::Parsing(const V<V<S>>& lines) {
+	for (size_t i = 0; i < lines.size(); ++i) {
+		const V<S>& line = lines[i];
+
+		if (std::find(line.begin(), line.end(), "") != line.end()) {
+			// UNREACHABLE
+			throw "Something goes wrong with reading. Contact to developers!\n";
+		}
+
+		auto error = parse_line(line);
+		if (error.id == Error::ErrorsId::NONE) {
+			equations.push_back(line);
+		}
+		else {
+			// update and save new error
+			error.set_index(i);
+			errors.push_back(error);
+		}
+	}
+}
--- Zhegalkin.h
+++ Zhegalkin.h
@@ -9,20 +9,6 @@
 #include <iostream>
 
 class Parsing {
-	enum Errors {
-		BIT = -1,
-		NONE,
-		IGNORE, INCOMPATIBILITY,
-		GENERAL, NUMBER, DUPLICATE,
-		EMPTY
-	};
-
-	std::string get_error(int code);
-
-	int parse_table(const std::vector<std::string>& tokens);
-	int parse_polynom(const std::vector<std::string>& tokens);
-	int parse_line(const std::vector<std::string>& line);
-
 protected:
 	enum Actions {
 		EQUAL = '=',
@@ -30,6 +16,35 @@ protected:
 		NEGATIVE = '0',
 		POSITIVE = '1'
 	};
+
+	struct Error {
+		enum ErrorsId {
+			BIT = -1,
+			NONE,
+			IGNORE, INCOMPATIBILITY,
+			GENERAL, NUMBER, DUPLICATE,
+			EMPTY
+		};
+		static std::string id2str(ErrorsId id);
+
+		std::string info;
+		ErrorsId id;
+		int index;
+		bool is_Table;
+
+		Error(ErrorsId _id, bool _is_Table, const std::string& _info);
+		void set_index(int index);
+
+		std::string get_error() const;
+	};
+
+	// storage for errors
+	std::vector<Error> errors;
+
+private:
+	Error parse_table(const std::vector<std::string>& tokens);
+	Error parse_polynom(const std::vector<std::string>& tokens);
+	Error parse_line(const std::vector<std::string>& line);
 
 public:
 	// storage for variable names
@@ -45,6 +60,8 @@ public:
 	std::vector<unsigned int> coefficients;
 
 	System_Equations(const std::vector<std::vector<std::string>>& lines) : Parsing(lines) {
+		for (const auto& it : errors)
+			std::cout << it.get_error() + "\n";
 		for (const auto& it : vocabulary)
 			std::cout << it << "\n";
 	}
