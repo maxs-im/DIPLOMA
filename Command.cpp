7d91ecc6f3eb0a5364c74068da44926e8edfdc7f
Maxim <maxim1998g@gmail.com>
2019-05-24 22:17:19 +0300

    New flag

--- Command.cpp
+++ Command.cpp
@@ -24,8 +24,10 @@ Options::Options() :
 	logs(false),
 	timer(false),
 	input_file(""),
-	output_file("")
+	output_file(""),
+	random(std::make_pair(0, 0))
 {}
+
 Options CMDHelper::work_with_cmd(const CommandParser& cmd) {
 	Options opt;
 	// help
@@ -46,6 +48,50 @@ Options CMDHelper::work_with_cmd(const CommandParser& cmd) {
 	if (cmd.is_exists(command)) {
 		opt.output_file = cmd.get_value(command);
 	}
+	command = "-rt";
+	if (cmd.is_exists(command)) {
+		opt.set_test(cmd.get_value(command));
+	}
 
 	return opt;
+}
+
+bool Options::is_testing() const {
+	return random.first > 0 && random.second > 0;
+}
+
+void Options::set_test(const std::string& str) {
+	std::string buffer = "";
+	for (size_t i = 0; i < str.length(); buffer += str[i], ++i) {
+		if (str[i] == ':' || (i + 1) == str.length()) {
+			if (buffer != "") {
+				try {
+					size_t num = std::stoull(buffer);
+					if (num == 0) {
+						throw "";
+					}
+
+					if (random.first == 0) {
+						random.first = num;
+					}
+					else if (random.second == 0) {
+						random.second = num;
+						break;
+					}
+				}
+				catch (...) {}
+
+				buffer = "";
+			}
+		}
+	}
+
+	// default value for flag
+	size_t def_val = 50;
+	if (random.first == 0) {
+		random.first = def_val;
+	}
+	if (random.second == 0) {
+		random.second = def_val;
+	}
 }
