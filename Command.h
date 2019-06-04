db2bde809b62b459d29ba5e159778b513da31ee0
Maxim <maxim1998g@gmail.com>
2019-06-04 06:07:54 +0300

    Added new command

--- Command.h
+++ Command.h
@@ -16,6 +16,7 @@ struct Options {
 	bool help, logs, timer, prefer_universal;
 	// variables : system
 	std::pair<size_t, size_t> random;
+	long long step;
 	std::string input_file;
 	std::string output_file;
 
@@ -23,6 +24,8 @@ struct Options {
 
 	bool is_testing() const;
 	void set_test(const std::string& str);
+
+	size_t get_index(size_t index) const;
 };
 
 namespace CMDHelper {
